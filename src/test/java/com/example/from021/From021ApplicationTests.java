c2c0a18c6c98d5017e5662d5e12b1f26f65ac848
ryan <zrtust@163.com>
2023-01-17 20:59:11 +0800

    1

---
+++ src/test/java/com/example/from021/From021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.from021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class From021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
