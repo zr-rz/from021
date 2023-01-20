0e7976b19a6869ae90418cf8415899d467f7f49a
ryan <zrtust@163.com>
2023-01-20 22:20:55 +0800

    zookeeper

---
+++ zookeeper/src/main/java/com/example/zookeeper/demo/ZkTest.java
@@ -0,0 +1,5 @@
+package com.example.zookeeper.demo;
+
+public class ZkTest {
+
+}
---
+++ zookeeper/src/test/java/com/example/zookeeper/TailTicket.java
@@ -0,0 +1,9 @@
+package com.example.zookeeper;
+
+public class TailTicket implements Runnable{
+    private int tickets = 10;
+    @Override
+    public void run() {
+
+    }
+}
